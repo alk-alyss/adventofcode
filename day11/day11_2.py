19e79e1f832f463422ed36ea5d9ddacfc9bd3437
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2021-12-11 08:10:57 +0200

    add day 11

---
+++ day11/day11_2.py
@@ -0,0 +1,38 @@
+with open("input.txt", "r") as f:
+	grid = []
+	for line in f.readlines():
+		grid.append([int(x) for x in line.strip()])
+
+def nextStep(grid: list):
+	res = 0
+	nextGrid = [[x for x in line] for line in grid]
+	
+	for i in range(len(grid)):
+		for j in range(len(grid[i])):
+			nextGrid[i][j] = grid[i][j]+1
+
+	while any([x>9 for line in nextGrid for x in line]):
+		for i in range(len(nextGrid)):
+			for j in range(len(nextGrid[i])):
+				if nextGrid[i][j] > 9:
+					res += 1
+					for k in range(-1, 2):
+						for l in range(-1, 2):
+							if k == 0 and l == 0: continue
+							if i+k < 0 or j+l < 0: continue
+							try:
+								if nextGrid[i+k][j+l] == 0: continue
+								nextGrid[i+k][j+l] += 1
+							except:
+								continue
+					nextGrid[i][j] = 0
+	
+	return res==len(grid)*len(grid[0]), nextGrid
+
+i = 0
+while True:
+	i += 1
+	temp, grid = nextStep(grid)
+	if temp: break
+
+print(i)
